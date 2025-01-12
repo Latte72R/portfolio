87fc65ef059a1e0fe2a7fd79e6dd321cca00df4b
Latte72 <ryofuji2005@gmail.com>
2025-01-12 20:16:12 +0900

    Update page.tsx

--- app/works/page.tsx
+++ app/works/page.tsx
@@ -27,7 +27,7 @@ const WorksPage: React.FC = () => {
                 <section id="turtle-graphics">
                     <h2>Turtle graphics</h2>
                     <p><Image id="image-turtle" className="works-image" src={TurtleImage} alt="Turtle Graphics Image" priority /></p>
-                    <p>Documents：<Link href="https://latte72r.github.io/TurtleGraphics/" target="_blank">https://latte72r.github.io/TurtleGraphics/</Link></p>
+                    <p>Documents：<Link href="https://turtle.latte72.net/" target="_blank">https://turtle.latte72.net/</Link></p>
                     <p>GitHub：<Link href="https://github.com/latte72r/TurtleGraphics/" target="_blank">https://github.com/latte72r/TurtleGraphics/</Link></p>
                     <p>Turtle graphics は、教育現場で教師がプログラミングの基礎を教えるために作られたツールです。</p>
                     <p>このサイトでは、JavaScript を使ってウェブブラウザから簡単に Turtle Graphics を体験できるようにアレンジしました。</p>
@@ -38,7 +38,7 @@ const WorksPage: React.FC = () => {
                 <section id="shilitori">
                     <h2>ShilitoriJS</h2>
                     <p><Image id="image-shilitori" className="works-image" src={ShilitoriImage} alt="Shilitori Image" /></p>
-                    <p>Documents：<Link href="https://latte72r.github.io/ShilitoriJS/" target="_blank">https://latte72r.github.io/ShilitoriJS/</Link></p>
+                    <p>Documents：<Link href="https://shilitori.latte72.net/" target="_blank">https://shilitori.latte72.net/</Link></p>
                     <p>GitHub：<Link href="https://github.com/latte72r/Shilitori/" target="_blank">https://github.com/latte72r/Shilitori/</Link></p>
                     <p>ShilitoriJS は、JavaScript を使用した「しりとり」ゲームです。</p>
                     <p>シンプルで直感的なインターフェースを採用していて、誰でも簡単に楽しむことができます。</p>
@@ -57,7 +57,7 @@ const WorksPage: React.FC = () => {
                 <section id="easy-turtle">
                     <h2>EasyTurtle</h2>
                     <p><Image id="image-easyturtle" className="works-image" src={EasyTurtleImage} alt="EasyTurtle Image" /></p>
-                    <p>Documents：<Link href="https://latte72r.github.io/EasyTurtle/" target="_blank">https://latte72r.github.io/EasyTurtle/</Link></p>
+                    <p>Documents：<Link href="https://easyturtle.latte72.net/" target="_blank">https://easyturtle.latte72.net/</Link></p>
                     <p>GitHub：<Link href="https://github.com/latte72r/EasyTurtle/" target="_blank">https://github.com/latte72r/EasyTurtle/</Link></p>
                     <p>EasyTurtle は、GUI を利用して Python の Turtle モジュールを簡単に扱えるプログラムです。</p>
                     <p>Windows と Linux 環境で動作し、値の入力とクリックだけでプログラミングが可能です。</p>
@@ -67,7 +67,7 @@ const WorksPage: React.FC = () => {
                 <section id="lsb">
                     <h2>Latte&apos;s Simple Browser</h2>
                     <p><Image id="image-lsb" className="works-image" src={LsbImage} alt="LSB Image" /></p>
-                    <p>Documents：<Link href="https://latte72r.github.io/LSB/" target="_blank">https://latte72r.github.io/LSB/</Link></p>
+                    <p>Documents：<Link href="https://lsb.latte72.net/" target="_blank">https://lsb.latte72.net/</Link></p>
                     <p>GitHub：<Link href="https://github.com/latte72r/LSB/" target="_blank">https://github.com/latte72r/LSB/</Link></p>
                     <p>Latte&apos;s Simple Browser ( LSB ) は、独自に開発されたシンプルで軽量なブラウザです。</p>
                     <p>このプロジェクトは、ブラウザの基本機能を学びながら実装することを目的として設計されています。</p>
