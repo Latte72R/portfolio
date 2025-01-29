04461f4eb602fa30f5946e7a663a5b92e560d4a7
Latte72 <ryofuji2005@gmail.com>
2025-01-30 04:13:20 +0900

    Create manifest.ts

---
+++ app/manifest.ts
@@ -0,0 +1,21 @@
+// app/manifest.ts
+import type { MetadataRoute } from 'next';
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: 'Latte72',
+    short_name: 'Latte72',
+    description: "Latte72's portfolio",
+    start_url: '/',
+    display: 'standalone',
+    background_color: '#223',
+    theme_color: '#223',
+    icons: [
+      {
+        src: '/latte_space.png',
+        sizes: '256x256',
+        type: 'image/png',
+      },
+    ],
+  };
+}
